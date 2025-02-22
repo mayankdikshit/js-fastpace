d0a7f7399df4bef219ffcb46e0f5418ae9e8f319
mayankdikshit <mayankdikshit711@gmail.com>
2025-02-22 22:11:08 +0530

    if-else done

--- 02_array_object.js
+++ 02_array_object.js
@@ -24,39 +24,4 @@ function preparechai(type) {
 preparechai("masalaChai");
 preparechai("gingerChai");
 
-function calculateTotal(amount) {
-  // covert to number
-  if (amount >= 1000) {
-    return amount * 0.9;
-  } else {
-    return amount;
-  }
-}
 
-// function calculateTotal(amount) {
-//     // covert to number
-//     if (amount >= 1000){
-//         return amount * 0.9
-//     }
-//      return amount
-// }
-
-// <---- switch case ---->
-
-function trafficLight(color) {
-  switch (color) {
-    case "red":
-      console.log("Stop");
-      break;
-    case "yellow":
-      console.log("Slow Down");
-      break;
-    case "green":
-      console.log("Go");
-      break;
-    default:
-      console.log("chalan kaat lo");
-  }
-}
-
-trafficLight("yellow");
--- 03_if_else.js
+++ 03_if_else.js
@@ -1,11 +1,14 @@
 function calculateTotal(amount) {
     // covert to number
-    if (amount >= 1000) {
-      return amount * 0.9;
-    } else {
-      return amount;
-    }
-  }
+//     if (amount >= 1000) {
+//       return amount * 0.9;
+//     } else {
+//       return amount;
+//     }
+//   }
+
+  return amount > 100 ? amount * 0.9 : amount;  // ternary opeartor --->
+}
   
   // function calculateTotal(amount) {
   //     // covert to number
@@ -34,3 +37,16 @@ function calculateTotal(amount) {
   }
 
   trafficLight("yellow");
+
+
+
+  function login(username, password){
+    if(username === "admin" && (password === "1234" || loginIp == "127")){
+        console.log("login successful");
+    } else {
+        console.log("invalid credentials");
+        
+    }
+  }
+
+  login("admin","1234");
