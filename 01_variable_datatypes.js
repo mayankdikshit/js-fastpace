4a61f57f671d6e681e8968c2794404a7de1240b1
mayankdikshit <mayankdikshit711@gmail.com>
2025-02-22 21:01:00 +0530

    file renamed

---
+++ 01_variable_datatypes.js
@@ -0,0 +1,34 @@
+let number = 42; // Number
+let name = "mayank"; // String
+let istrue = true; // Boolean
+
+let nothing = null ; // object // EXCEPTION ??
+let undefinedVar = undefined; // undefined
+let symbolvar = Symbol(); //symbol
+
+
+// console.log(typeof(nothing));
+// console.log(typeof(undefinedVar));
+
+let person = {
+    name: "mayank",
+    age: 25,
+    isStudent: true,
+}
+
+let num = 42;
+let covertedNumber = Number(num);
+console.log(covertedNumber);
+
+
+
+console.log(Math.max(5,4));
+console.log(Math.min(5,4));
+console.log(Math.random() * 10);
+
+
+Math.floor
+
+
+
+
