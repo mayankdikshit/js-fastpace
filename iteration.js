ef62ce8ccd5dabfb43944f8fd71309fdb82a478b
mayankdikshit <mayankdikshit711@gmail.com>
2025-02-24 18:20:39 +0530

    functions added

--- iteration.js
+++ iteration.js
@@ -17,3 +17,17 @@ let  userActivity = [
 
 // let mostActiveUser = userActivity.reduce(() => ())
 
+  let expenses = [
+    { description: "Groceries", amount: 50, category: "Food" },
+    { description: "Electricity Bill", amount: 100, category: "Utilities" },
+    { description: "Dinner", amount: 30, category: "Food" },
+    { description: "Internet Bill", amount: 50, category: "Utilities" },
+  ];
+  
+  let expenseReport = expenses.reduce((report, expense) => {
+    report[expense.category] = (report[expense.category] || 0) + expense.amount;
+    return report;
+  }, {});
+  
+  console.log("Expense Report", expenseReport);
+  
