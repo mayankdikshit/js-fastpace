8023747aea0024ebc9e39c726304d61864cc43da
mayankdikshit <mayankdikshit711@gmail.com>
2025-02-26 00:18:32 +0530

    reduce added

---
+++ map-filter-reduce.js
@@ -0,0 +1,46 @@
+// let array = [2 , 4, 6, 8, 10];
+
+// // double
+
+// // tripple
+
+// function double(x) {
+//     return x*2;
+// }
+
+// const doubleArray = array.map(double);
+// console.log(doubleArray);
+
+// function tripple(y){
+//     return y*3
+// }
+
+// const trippleArray = array.map(tripple);
+// console.log(trippleArray);
+
+const arr = [5, 1, 3, 2, 6];
+
+// filter odd values
+
+// function greaterFour(x){
+//     return x > 4;
+//     }
+
+// const output = arr.filter((x) => (x>4))
+
+// console.log(output);
+
+//  (function chai(){})();
+
+function sayMyName(name){
+    console.log(name)
+}
+
+sayMyName("mayank");
+
+// ------------------ //
+
+function sayMyName(name) {
+  console.log(name);
+}
+
